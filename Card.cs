a7cb476241120fdbff64dba03c2b1e76b8517d65
frederico.101 <fredyalves.fredyalves@hotmail.com>
2020-02-23 16:17:20 -0300

    estrutura basica do linq

---
+++ Card.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+namespace Linq
+{
+    public static class Card
+    {
+        // Program.cs
+// The Main() method
+
+public static IEnumerable<string> Suits()
+{
+    yield return "clubs";
+    yield return "diamonds";
+    yield return "hearts";
+    yield return "spades";
+}
+
+public static IEnumerable<string> Ranks()
+{
+    yield return "two";
+    yield return "three";
+    yield return "four";
+    yield return "five";
+    yield return "six";
+    yield return "seven";
+    yield return "eight";
+    yield return "nine";
+    yield return "ten";
+    yield return "jack";
+    yield return "queen";
+    yield return "king";
+    yield return "ace";
+}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var startingDeck = from s in Card.Suits()
+                               from r in Card.Ranks()
+                               select new { Suit = s, Rank = r };
+
+            // Display each card that we've generated and placed in startingDeck in the console
+            foreach (var card in startingDeck)
+            {
+                Console.WriteLine(card);
+            }
+        }
+
+
+    }
+}
